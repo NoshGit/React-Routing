64162ba72c214a6c0029efb910617f316ca81446
Noshir Patel <noshirpatel86@gmail.com>
2021-09-11 14:29:35 +0530

    "page not found implemented"

--- src/Components/ErrorComponent.js
+++ src/Components/ErrorComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ErrorComponent = () => {
     return (
         <div style={{padding:'20px'}}>
-            <img style={{width:'50vw', height:'auto'}} src={`./../assets/404-error-page-not-found.jpg`} />
+            <img style={{width:'50vw', height:'auto'}} src={`assets/404-error-page-not-found.jpg`} />
         </div>
     )
 }
